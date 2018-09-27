d33b92cd79a0e8fe4262fbe858105e7f2d55847e
GCrown <gcrownlin@gmail.com>
2018-09-27 23:48:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gcrown/textrecognition/module/MainPresenter.java
@@ -0,0 +1,147 @@
+package com.example.gcrown.textrecognition.module;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+import android.util.Log;
+
+import com.example.gcrown.textrecognition.apiservice.BaiduOCRService;
+import com.example.gcrown.textrecognition.bean.AccessTokenBean;
+import com.example.gcrown.textrecognition.bean.RecognitionResultBean;
+import com.example.gcrown.textrecognition.utils.RegexUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author chaochaowu
+ * @Description : MainPresenter
+ * @class : MainPresenter
+ * @time Create at 6/4/2018 4:21 PM
+ */
+
+
+public class MainPresenter implements MainContract.Presenter{
+
+    private MainContract.View mView;
+    private BaiduOCRService baiduOCRService;
+
+    private static final String CLIENT_CREDENTIALS = "client_credentials";
+    private static final String API_KEY = "p9dGfw5WqTjU881qgRArrg0k";
+    private static final String SECRET_KEY = "w2DPzH6YTtqswKgtiEf9GUzpmKPkw3pN";
+    private static final String ACCESS_TOKEN = "23.f157908e55802eaae1ca590ef891ec42.2592000.1540568681.2218671224-11688287";
+
+    public MainPresenter(MainContract.View mView) {
+
+        this.mView = mView;
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://aip.baidubce.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        baiduOCRService = retrofit.create(BaiduOCRService.class);
+
+    }
+
+
+    @Override
+    public void getAccessToken() {
+
+        baiduOCRService.getAccessToken(CLIENT_CREDENTIALS,API_KEY,SECRET_KEY)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<AccessTokenBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(AccessTokenBean accessTokenBean) {
+                        Log.e("Access token",accessTokenBean.getAccess_token());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("v","error");
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+
+
+    @Override
+    public void getRecognitionResultByImage(Bitmap bitmap) {
+
+        String encodeResult = bitmapToString(bitmap);
+
+        baiduOCRService.getRecognitionResultByImage(ACCESS_TOKEN,encodeResult)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<RecognitionResultBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(RecognitionResultBean recognitionResultBean) {
+                        Log.e("onnext",recognitionResultBean.toString());
+
+                        ArrayList<String> wordList = new ArrayList<>();
+                        List<RecognitionResultBean.WordsResultBean> wordsResult = recognitionResultBean.getWords_result();
+                        for (RecognitionResultBean.WordsResultBean words:wordsResult) {
+                            wordList.add(words.getWords());
+                        }
+
+                        //ArrayList<String> numbs = RegexUtils.getNumbs(wordList);
+                        StringBuilder s = new StringBuilder();
+
+                        for (String numb : wordList) {
+                            s.append(numb + "\n");
+
+                        }
+
+                        mView.updateUI(s.toString());
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("onerror",e.toString());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+
+
+    private String bitmapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes = baos.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+
+}
